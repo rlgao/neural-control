86950446d0ed22709502ecb169c64f78e5cf89c1
rlgao <1393307292@qq.com>
2022-12-15 17:13:11 +0800

    Completed ex1

--- ex1_bp.m
+++ ex1_bp.m
@@ -1,6 +1,6 @@
+% ex1_bp.m
 clc;clear;close all;
 
-% function DualModelIden
 %% 参数初始化
 l = 0.009;    % 学习率
 alfa = 0.05;  % 动量因子
@@ -8,7 +8,7 @@ alfa = 0.05;  % 动量因子
 cells1 = 20;  % 隐层神经元个数
 cells2 = 10;
 
-w1 = rand(cells1,2);       % 随机赋值第一层连接权系数 [20  2 ]
+w1 = rand(cells1,3);       % 随机赋值第一层连接权系数 [20  3]
 w2 = rand(cells2,cells1);  % 随机赋值第二层连接权系数 [10  20]
 w3 = rand(1,cells2);       % 随机赋值第三层连接权系数 [1   10]
 
@@ -16,17 +16,21 @@ yw1 = rand(cells1,1);  % 随机赋值第一层输出阈值 [20  1]
 yw2 = rand(cells2,1);  % 随机赋值第二层输出阈值 [10  1]
 yw3 = rand;            % 随机赋值第三层输出阈值 [1]
 
-ts = 0.001; % 样本
-n  = 1000;  % 样本数
+ts = 0.001;
+%%%%%%%%%%%%%%%%%%%%%%%%%%
+n = 500;  % 样本数
+% n = 1000;  % 样本数
+%%%%%%%%%%%%%%%%%%%%%%%%%%
 yn = rand(1,n);  % 随机赋值输出(预测）
 y  = rand(1,n);  % 随机赋值输出(真实）
 
 counts = 1;  % 计数值初始化
 
-x = [0,0]';  % 输入
+x = [0,0,0]';  % 输入
 
 u_1 = 0;  % 上一时刻的输入
 y_1 = 0;  % 上一时刻的输出
+y_2 = 0;  % 上上一时刻的输出
 
 times = 300;  % 训练轮数
 e = zeros(1,times);  % 均方差初始值设为0
@@ -36,8 +40,15 @@ for i = 1:times  % 学习轮数
     ei = 0;
     for a = 1:n  % 样本数
         time(a) = a*ts;
-        u(a) = 0.50*sin(3*2*pi*a*ts);
-        y(a) = y_1 / (1+y_1^2) + u_1^3;
+        
+        %%%%%%%%%%%%%%%%%%%%%%%%%%
+        % 系统输入
+%         u(a) = 0.50*cos(6*pi*a*ts);
+        u(a) = -0.75*sin(12*pi*a*ts);
+        %%%%%%%%%%%%%%%%%%%%%%%%%%
+        
+        % 系统真实模型
+        y(a) = (y_1 - y_2) / sqrt(1 + y_1^2) + u_1^3;
         
         net1 = w1*x - yw1;     % 第一层网络的输入 [20, 1]
         out1 = logsig(net1);   % 第一层网路的输出 [20, 1]
@@ -61,11 +72,15 @@ for i = 1:times  % 学习轮数
         ei = ei + det3^2 / 2;
         e(i) = ei;
         
-        x(1) = u(a);  % 更新输入
+        % 更新输入
+        x(1) = u(a);
         x(2) = y(a);
-    
+        x(3) = y_1;
+        
+        y_2 = y_1;
         u_1 = u(a);
         y_1 = y(a);
+        
     end  % 结束一次样本遍历
     
     if ei < 0.008
@@ -74,43 +89,17 @@ for i = 1:times  % 学习轮数
     counts = counts + 1;
 end  % 结束学习
 
-%% 计算学习的曲线
-x = [0,0]';  % 输入
-yn_test = rand(1,n);  % 随机赋值输出(预测)
-y_test  = rand(1,n);  % 随机赋值输出(真实)
-u_1 = 0;  % 上一时刻的输入
-y_1 = 0;  % 上一时刻的输出
-ts = 0.1;  % 样本
-n  = 1000; % 样本数
-
-for a = 1:n
-    u(a) = sin(2*pi*a*ts/25) + sin(2*pi*a*ts/10);
-    y_test(a) = u_1^3 + y_1 / (1+y_1^2);
- 
-    net1 = w1*x - yw1;
-    out1 = logsig(net1);
-    net2 = w2*out1 - yw2;
-    out2 = logsig(net2);
-    net3 = w3*out2 - yw3;
-    yn_test(a) = net3;
-    
-	x(1)=u(a);
-	x(2)=y_test(a);
-    
-	u_1 = u(a);
-	y_1 = y_test(a);
-end
-
 %% 绘图
 figure(1);
 subplot(2,1,1);
 plot(time,y,'b-',time,yn,'r-');
-legend('true', 'forecast')
+legend('真实模型输出y', 'BP网络输出y')
 grid on
-title('BP学习方法逼近y=0.5*(1+cos(x))');
-xlabel('x轴');
-ylabel('y=0.5*(1+cos(x))');
+title('输出响应');
+xlabel('t');
+ylabel('y');
 
+counts = counts - 1;
 if (counts < times)
     count = 1:counts;
     sum = counts;
@@ -120,18 +109,10 @@ else
 end
 
 subplot(2,1,2);
-plot(count,e(1:sum));
+plot(count,e(1:sum),'r-');
 grid on;
-title('BP算法学习曲线');
+title('学习曲线');
 xlabel('迭代次数');
-ylabel('Mean-Square-Error');
+ylabel('MSE');
 
-
-figure(2);
-plot(time,y_test,'b-',time,yn_test,'r-');
-legend('true', 'forecast')
-grid on
-title('BP学习方法逼近y=sin(2*pi*k/25) + sin(2*pi*k/10)');
-xlabel('x轴');
-ylabel('y=sin(2*pi*k/25) + sin(2*pi*k/10)');
-% return
+% END
